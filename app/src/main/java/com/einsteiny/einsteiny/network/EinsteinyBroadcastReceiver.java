a0a3894641616b04aca5e841ae9b159db4a47bce
Antonina Cherednichenko <antonina.cherednichenko@gmail.com>
2017-04-17 12:14:49 -0700

    add progress tracking

--- app/src/main/java/com/einsteiny/einsteiny/network/EinsteinyBroadcastReceiver.java
+++ app/src/main/java/com/einsteiny/einsteiny/network/EinsteinyBroadcastReceiver.java
@@ -13,6 +13,8 @@ import com.einsteiny.einsteiny.R;
 import com.einsteiny.einsteiny.activities.PlayYoutubeActivity;
 import com.einsteiny.einsteiny.models.Course;
 import com.einsteiny.einsteiny.models.Course_Table;
+import com.einsteiny.einsteiny.models.CustomUser;
+import com.einsteiny.einsteiny.models.Lesson;
 import com.raizlabs.android.dbflow.sql.language.SQLite;
 
 import org.json.JSONException;
@@ -44,12 +46,14 @@ public class EinsteinyBroadcastReceiver extends BroadcastReceiver {
                     // Extract custom push data
                     if (key.equals("course")) {
                         String courseId = value;
-
+                        int progress = CustomUser.getProgressForCourse(courseId);
                         Course course = SQLite.select().
                                 from(Course.class).where(Course_Table.id.is(courseId)).querySingle();
+                        Lesson lesson = course.getMyLessons().get(progress);
+                        CustomUser.addProgressForCourse(courseId);
 
                         Intent intentActivity = new Intent(context, PlayYoutubeActivity.class);
-                        intentActivity.putExtra(PlayYoutubeActivity.EXTRA_LESSON, "");
+                        intentActivity.putExtra(PlayYoutubeActivity.EXTRA_LESSON, lesson.getVideoUrl());
 
 
                         int requestID = (int) System.currentTimeMillis(); //unique requestID to differentiate between various notification with same NotifId
@@ -58,7 +62,7 @@ public class EinsteinyBroadcastReceiver extends BroadcastReceiver {
                         Notification noti = new NotificationCompat.Builder(context.getApplicationContext())
                                 .setSmallIcon(R.drawable.ic_explore)
                                 .setContentTitle("Hello clever!")
-                                .setContentText("Time to watch new video...")
+                                .setContentText("Time to watch new video for course " + course.getTitle())
                                 .setContentIntent(pIntent)
                                 .setAutoCancel(true) // Hides the notification after its been selected
                                 .build();
